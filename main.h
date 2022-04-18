5848615ec9d586f4a58965dec3f22a3dc48cad84
fiansky <samuel.asare@azubiafrica.org>
2022-04-18 19:18:19 +0000

    completed main.h, _printf.c

--- main.h
+++ main.h
@@ -11,3 +11,12 @@ int print_num(long int n);
 int print_string(char *str);
 int print_binary(unsigned int num);
 int print_unknown_spec(char c);
+int print_odh(char c, unsigned int num);
+int dec_to_oct(unsigned int num);
+int dec_to_hex(char c, unsigned int num);
+int print_S(char *);
+int print_reverse(char *s);
+int print_rot13(char *c);
+int print_pointer(void *p);
+
+#endif
